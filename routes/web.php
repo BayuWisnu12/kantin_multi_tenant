0d32e7372b0383869720a544921b97938fd49ca1
BayuWisnu12 <bayuwisnu1771@gmail.com>
2026-09-09 20:04:30 +0700

    style: format web.php code style using pint

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 require __DIR__.'/settings.php';
 
 // --- TAMBAHKAN KEMBALI BARIS INI UNTUK BYPASS ERROR PASSKEY ---
-Route::get('/passkey-dummy-login-options', function() {})->name('passkey.login-options');
-Route::post('/passkey-dummy-login', function() {})->name('passkey.login');
-Route::get('/passkey-dummy-confirm-options', function() {})->name('passkey.confirm-options');
-Route::post('/passkey-dummy-confirm', function() {})->name('passkey.confirm');
+Route::get('/passkey-dummy-login-options', function () {})->name('passkey.login-options');
+Route::post('/passkey-dummy-login', function () {})->name('passkey.login');
+Route::get('/passkey-dummy-confirm-options', function () {})->name('passkey.confirm-options');
+Route::post('/passkey-dummy-confirm', function () {})->name('passkey.confirm');
