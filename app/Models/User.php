376e61657b3349802bc5571d003f49f9ddcc694f
BayuWisnu12 <bayuwisnu1771@gmail.com>
2026-09-09 21:38:19 +0700

    fix: memperbaiki import trait pada model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\PasskeyUser;
 use Laravel\Fortify\PasskeyAuthenticatable;
+use App\Concerns\HasTeams;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-
 /**
  * @property int $id
  * @property string $name
@@ -35,7 +35,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable implements PasskeyUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
+    use HasFactory, HasTeams, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
 
     /**
      * Get the attributes that should be cast.
